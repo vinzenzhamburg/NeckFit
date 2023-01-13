e27556b176263c810ed0411755f524fc87de368f
vinzenzharm <115456282+vinzenzharm@users.noreply.github.com>
2023-01-13 15:48:41 +0100

    Bearbeitungs Commit mit Bayram Tokses

---
+++ app/src/main/java/com/example/neckfit/ui/authentication/LoginFragment.kt
@@ -0,0 +1,65 @@
+package com.example.neckfit.ui.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.neckfit.R
+import com.example.neckfit.databinding.FragmentLoginBinding
+import com.example.neckfit.ui.MainViewModel
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding : FragmentLoginBinding
+
+    private val viewModel : MainViewModel by activityViewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        viewModel.toast.observe(viewLifecycleOwner) {
+            if (it != null) {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        viewModel.currentUser.observe(viewLifecycleOwner) {
+            if (it != null) {
+                findNavController().navigate(R.id.fragment_home)
+            }
+        }
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.loginEmailEdit.text.toString()
+            val password = binding.loginPasswordEdit.text.toString()
+
+            if (!email.isNullOrEmpty() && !password.isNullOrEmpty()) {
+                viewModel.login(email, password)
+            }
+        }
+
+        binding.loginSignupButton.setOnClickListener {
+            findNavController()
+                .navigate(LoginFragmentDirections.actionLoginFragmentToSignupFragment())
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/neckfit/ui/authentication/SignupFragment.kt
@@ -0,0 +1,66 @@
+package com.example.neckfit.ui.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewTreeLifecycleOwner
+import androidx.navigation.fragment.findNavController
+import com.example.neckfit.R
+import com.example.neckfit.databinding.FragmentLoginBinding
+import com.example.neckfit.databinding.FragmentSignupBinding
+import com.example.neckfit.ui.MainViewModel
+
+class SignupFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    private lateinit var binding : FragmentSignupBinding
+
+    private val viewModel : MainViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+            binding = DataBindingUtil.inflate(inflater,R.layout.fragment_signup,container,false)
+
+        return binding.root
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.toast.observe(viewLifecycleOwner) {
+            if (it != null) {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        binding.signupSignupButton.setOnClickListener {
+            signUp()
+        }
+        binding.signupCancelButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun signUp() {
+
+        val email = binding.signupMail.text.toString()
+        val password = binding.signupPassword.text.toString()
+
+        if(!email.isNullOrEmpty() && !password.isNullOrEmpty()) {
+            viewModel.signUp(email,password)
+        }
+    }
+
+}
