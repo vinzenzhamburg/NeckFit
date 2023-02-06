870251d5ccb43fb4b52b467be6453b0b90b2bf34
vinzenzharm <115456282+vinzenzharm@users.noreply.github.com>
2023-02-06 16:14:25 +0100

    Commit :
    Im Repo neue Function angelegt,
    
    Fun getAllFavorites.

--- app/src/main/java/com/example/neckfit/data/Repository.kt
+++ app/src/main/java/com/example/neckfit/data/Repository.kt
@@ -1,29 +1,34 @@
 package com.example.neckfit.data
 
-import com.example.neckfit.data.datamodel.*
+import android.provider.LiveFolders
+import androidx.lifecycle.LiveData
+import com.example.neckfit.data.datamodel.Theme
+import com.example.neckfit.data.datamodel.Training
+import com.example.neckfit.data.datamodel.Uebung
 import com.example.neckfit.data.local.TrainingsDatabase
-import com.example.neckfit.data.local.TrainingsDatabaseDao
 import com.example.neckfit.data.remote.NeckFitApi
 
 class Repository(private val database: TrainingsDatabase) {
 
-    suspend fun loadThemes() : List<Theme> {
+    suspend fun loadThemes(): List<Theme> {
         return NeckFitApi.retrofitService.getThemes()
     }
 
-    suspend fun loadExercises() : List<Uebung> {
+    suspend fun loadExercises(): List<Uebung> {
         return NeckFitApi.retrofitService.getExercises()
     }
 
-    suspend fun loadAllTraining() : List<Training> {
+    suspend fun loadAllTraining(): List<Training> {
         return NeckFitApi.retrofitService.getAllTraining()
     }
 
+    suspend fun getAllFavorite(): LiveData<List<Training>> =
+        database.trainingsDatabaseDao.getAllFavorites()
+
     suspend fun setFavorite(training: Training) {
-        if(training.favorite){
-        database.trainingsDatabaseDao.insert(training)
-        }else if (!training.favorite)
-        database.trainingsDatabaseDao.delete(training)
+        if (training.favorite) {
+            database.trainingsDatabaseDao.insert(training)
+        } else if (!training.favorite)
+            database.trainingsDatabaseDao.delete(training)
     }
 }
-
