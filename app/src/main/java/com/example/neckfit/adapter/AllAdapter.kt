f7d8b176c6b4ffe090c101c19515e6adc373da1b
vinzenzharm <115456282+vinzenzharm@users.noreply.github.com>
2023-02-06 16:11:02 +0100

    Commit :
    Den All-Adapter ausser Funktion gesetzt,
    
    und zum löschen vorbereitet.
    
    Fehlerbehebung des AllTrainAdapter damit er die ganze Arbeit des
    
    All-Adapter übernimmt.

--- app/src/main/java/com/example/neckfit/adapter/AllAdapter.kt
+++ app/src/main/java/com/example/neckfit/adapter/AllAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.neckfit.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,29 +12,29 @@ import com.example.neckfit.R
 import com.example.neckfit.data.datamodel.Training
 import com.example.neckfit.ui.MainViewModel
 
-class AllAdapter (private val mainViewModel : MainViewModel)
+/*class AllAdapter (private val mainViewModel : MainViewModel)
     : RecyclerView.Adapter<AllAdapter.ItemViewHolder>() {
 
     private var dataset: List<Training> = emptyList()
 
-
     fun submitList(allTraining: List<Training>) {
         dataset = allTraining
         notifyDataSetChanged()
-
     }
+
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
         val image: ImageView = view.findViewById(R.id.imageAllTrain)
         val description: TextView = view.findViewById(R.id.text_alltrain)
         val favoriteStar: ImageView = view.findViewById(R.id.favoriteStar)
+        val favoriteStarRed: ImageView = view.findViewById(R.id.favoriteStarRed)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllAdapter.ItemViewHolder {
+
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item_all_training, parent, false)
         return AllAdapter.ItemViewHolder(adapterLayout)
-
     }
 
     override fun getItemCount(): Int {
@@ -41,12 +42,19 @@ class AllAdapter (private val mainViewModel : MainViewModel)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+
         val training = dataset[position]
 
         holder.image.load(training.image)
         holder.description.text = training.description
         holder.favoriteStar.setOnClickListener{
             mainViewModel.setFavorite(training)
+
+        holder.favoriteStar.visibility = View.INVISIBLE
+        holder.favoriteStarRed.setImageResource(R.drawable.ic_baseline_star_25)
+        holder.favoriteStarRed.visibility = View.VISIBLE
         }
     }
 }
+
+ */
