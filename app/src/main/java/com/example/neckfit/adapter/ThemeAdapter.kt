c3865ce248528ef2abab4dc10581ecd34d2d5629
vinzenzharm <115456282+vinzenzharm@users.noreply.github.com>
2023-02-06 16:15:53 +0100

    Commit :
    Code-Zeilen gelöscht.

--- app/src/main/java/com/example/neckfit/adapter/ThemeAdapter.kt
+++ app/src/main/java/com/example/neckfit/adapter/ThemeAdapter.kt
@@ -1,7 +1,5 @@
 package com.example.neckfit.adapter
 
-
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +13,7 @@ import com.example.neckfit.ui.main.FragmentThemeDirections
 
 class ThemeAdapter(
     ) : RecyclerView.Adapter<ThemeAdapter.ItemViewHolder>() {
+
     private var dataset: List<Theme> = emptyList()
 
     fun submitList(list : List<Theme>){
@@ -22,35 +21,31 @@ class ThemeAdapter(
         notifyDataSetChanged()
     }
 
-    // IDEE EINES VIEWHOLDERS
-    // der ViewHolder weiß welche Teile des Layouts beim Recycling angepasst werden
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
         val name: TextView = view.findViewById(R.id.category_title_name)
         val themeButton: ConstraintLayout = view.findViewById(R.id.themen_item_Constrain)
     }
 
-    // ERSTELLEN DES VIEWHOLDERS
-    // hier werden neue ViewHolder erstellt
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item_themen, parent, false)
 
         return ItemViewHolder(adapterLayout)
     }
 
-    // BEFÜLLEN DES VIEWHOLDERS
-    // hier findet der Recyclingprozess statt
-    // die vom ViewHolder bereitgestellten Parameter werden verändert
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+
         val theme = dataset[position]
+
+        holder.name.text = theme.name
         holder.themeButton.setOnClickListener {
             holder.itemView.findNavController()
                 .navigate(FragmentThemeDirections.actionFragmentThemeToFragmentCategory(theme.name))
         }
-        holder.name.text = theme.name
     }
 
-    // damit der LayoutManager weiß wie lang die Liste ist
     override fun getItemCount(): Int {
         return dataset.size
     }
